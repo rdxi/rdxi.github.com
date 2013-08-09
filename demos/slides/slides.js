1cae3b426e5d288f008c6126010acbeb49d51f5a
rdxi <you@example.com>
2013-08-09 21:05:51 +0700

    added demos

---
+++ demos/slides/slides.js
@@ -0,0 +1,15 @@
+window.slides = [
+  { title: 'Use keyboard<br>&larr; left and right &rarr; to navigate!' },
+  { title: 'Hello this is slide 2'},
+  { title: 'This is slide 3<br>this is slide 3<br>this is slide 3<br>this is slide 3<br>this is slide 3<br>this is slide 3<br>this is slide 3'},
+  {
+    title: 'Slide 4 with bullets!',
+    bullets: ['Point One', 'Point Two', 'Point Three']
+  },
+  { image: 'http://placekitten.com/600/600'},
+  {
+    quote: 'This is slide quote',
+    cite: 'Slide Six'
+  }
+
+];
